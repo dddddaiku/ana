4157fa676a8a84c27ef83dea41b866e3e6a0d414
SOLIZE\kosuke.daiku <kosuke.daiku@solize.com>
2023-05-18 13:26:52 +0900

    #test

---
+++ Assets/testsc.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testsc : MonoBehaviour
+{
+    private Vector2 aaa;
+    private float aassd;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        tsww();
+    }
+    public void dan()
+    {
+        aaa = sss;
+    }
+}
